b6b7cb95cedbb1b7e117869161fca768f94a57ba
Raissa Vitória <123013855+raissabispo@users.noreply.github.com>
2023-04-30 22:30:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function toggleMode() {
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar.png")
      img.setAttribute("alt", "foto de Raissa Vitória com óculos.")
   } else {
     // se tiver sem light mode, manter a imagem normal
